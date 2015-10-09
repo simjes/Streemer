a7ecf26ee062ab6db255d16ad63ea81f425f3cc7
Simon Jespersen <s.jespersen@stud.uis.no>
2015-10-09 23:57:49 +0200

    games infinite scroll

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,17 +1,11 @@
 var channelList;
 var stream;
-//var chat;
-
+var numberOfGames = 0;
+var numberOfChannels = 0;
 
 angular.module('streemer.controllers', [])
   .controller('TwitchCtrl', function ($scope, $http, $sce) {
-    $scope.init = function () {
-      $http.get('https://api.twitch.tv/kraken/games/top?limit=12').then(function (result) {
-        $scope.data = angular.fromJson(result).data;
-      }, function (error) {
-        alert('error: ' + error.toString());
-      });
-    };
+    $scope.games = [];
 
     $scope.populateChannels = function () {
       $scope.channels = channelList.streams;
@@ -21,8 +15,15 @@ angular.module('streemer.controllers', [])
       return $sce.trustAsResourceUrl(stream);
     };
 
-    /*
-     $scope.getStreamChat = function () {
-     return $sce.trustAsResourceUrl(chat);
-     }; */
+    $scope.loadMoreGames = function () {
+      $http.get('https://api.twitch.tv/kraken/games/top?limit=6', {params: {offset: numberOfGames}}).then(function (result) {
+        numberOfGames += 6;
+        angular.forEach(angular.fromJson(result).data.top, function(game) {
+          $scope.games.push(game);
+        });
+        $scope.$broadcast('scroll.infiniteScrollComplete');
+      }, function (error) {
+        alert('error: ' + error.toString());
+      });
+    };
   });
--- www/js/directives/gameInfo.js
+++ www/js/directives/gameInfo.js
@@ -9,7 +9,8 @@ angular.module('streemer.gameInfo', [])
       templateUrl: 'templates/directive/gameInfo.html',
       controller: function ($scope, $http, $state) {
         $scope.goToGame = function (gameName) {
-          $http.get('https://api.twitch.tv/kraken/streams', {params: {game: gameName}}).then(function (result) {
+          $http.get('https://api.twitch.tv/kraken/streams?limit=10', {params: {game: gameName}}).then(function (result) {
+            numberOfChannels = 10;
             $state.go('twitchChannelList');
             channelList = angular.fromJson(result).data;
           }, function (error) {
