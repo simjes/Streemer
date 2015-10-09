bfeb8b687b27573e02ed9400bcf49413338eda1f
Simon Jespersen <s.jespersen@stud.uis.no>
2015-10-09 21:59:07 +0200

    temp drop chat

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 var channelList;
 var stream;
-var chat;
+//var chat;
 
 angular.module('streemer.controllers', [])
         .controller('TwitchCtrl', function ($scope, $http, $state, $sce) {
@@ -23,12 +23,12 @@ angular.module('streemer.controllers', [])
 
             $scope.populateChannels = function () {
                 $scope.channels = channelList.streams;
-            }
+            };
 
             $scope.goToChannel = function (channelName) {
                 $state.go('twitchStream');
                 stream = "http://www.twitch.tv/"+ channelName + "/embed";
-                chat = "http://www.twitch.tv/"+ channelName+ "/chat";
+                //chat = "http://www.twitch.tv/"+ channelName+ "/chat";
                 /* $http.get('https://api.twitch.tv/kraken/streams/' + channelName).then(function (result) {
                  $state.go('twitchStream');
                  $scope.streamer = channelName;
@@ -37,14 +37,15 @@ angular.module('streemer.controllers', [])
                  }, function (error) {
                  alert('error: ' + error.toString());
                  });*/
-            }
-            
+            };
+
             $scope.getStreamLink = function () {
                 return  $sce.trustAsResourceUrl(stream);
-            }
-            
+            };
+
+          /*
             $scope.getStreamChat = function () {
                 return $sce.trustAsResourceUrl(chat);
-            }
-            
+            }; */
+
         });
