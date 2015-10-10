fac4fe83a82638dfd228f453d0a27ce94dc9863d
Simon Jespersen <s.jespersen@stud.uis.no>
2015-10-10 20:07:29 +0200

    loading channels

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,9 +1,11 @@
 var stream;
 var numberOfGames = 0;
+var numberOfChannels = 0;
 
 angular.module('streemer.controllers', [])
   .controller('TwitchCtrl', function ($scope, $http, $sce) {
     $scope.games = [];
+    $scope.channels = [];
 
     $scope.getStreamLink = function () {
       return $sce.trustAsResourceUrl(stream);
@@ -12,7 +14,7 @@ angular.module('streemer.controllers', [])
     $scope.loadMoreGames = function () {
       $http.get('https://api.twitch.tv/kraken/games/top?limit=6', {params: {offset: numberOfGames}}).then(function (result) {
         numberOfGames += 6;
-        angular.forEach(angular.fromJson(result).data.top, function(game) {
+        angular.forEach(angular.fromJson(result).data.top, function (game) {
           $scope.games.push(game);
         });
         $scope.$broadcast('scroll.infiniteScrollComplete');
@@ -21,7 +23,25 @@ angular.module('streemer.controllers', [])
       });
     };
 
-    $scope.populateStuff = function() {
-      $scope.channels = channelList.streams;
-    };
+    $scope.loadMoreChannels = function () {
+      console.log($scope.channels);
+
+
+      $http.get('https://api.twitch.tv/kraken/streams?limit=6', {
+        params: {
+          game: currentGame,
+          offset: numberOfChannels
+        }
+      }).then(function (result) {
+        console.log(result);
+        angular.forEach(angular.fromJson(result).data.streams, function (channel) {
+          $scope.channels.push(channel);
+        });
+        numberOfChannels += 6;
+        $scope.$broadcast('scroll.infiniteScrollComplete');
+      }, function (error) {
+        alert('error: ' + error.toString());
+      });
+    }
+
   });
--- www/js/directives/gameInfo.js
+++ www/js/directives/gameInfo.js
@@ -1,6 +1,4 @@
 var currentGame = "";
-var numberOfChannels = 0;
-var channelList;
 
 angular.module('streemer.gameInfo', [])
   .directive('gameInfo', function () {
@@ -12,30 +10,11 @@ angular.module('streemer.gameInfo', [])
       replace: true,
       templateUrl: 'templates/directive/gameInfo.html',
       controller: function ($scope, $http, $state) {
-        $scope.channels = [];
-
         $scope.goToGame = function (gameName) {
-          $http.get('https://api.twitch.tv/kraken/streams', {params: {game: gameName}}).then(function (result) {
-            $state.go('twitchChannelList');
-            channelList = angular.fromJson(result).data;
-          }, function (error) {
-            alert('error: ' + error.toString());
-          });
+          currentGame = gameName;
+          numberOfChannels = 0;
+          $state.go('twitchChannelList');
         };
-
-
-        /*$scope.loadMoreChannels = function () {
-          console.log("loading.....");
-          $http.get('https://api.twitch.tv/kraken/streams?limit=6', {params: {game: currentGame, offset: numberOfGames}}).then(function (result) {
-            numberOfChannels += 6;
-            angular.forEach(angular.fromJson(result).data, function (channel) {
-              $scope.channels.push(channel);
-            });
-            $scope.$broadcast('scroll.infiniteScrollComplete');
-          }, function (error) {
-            alert('error: ' + error.toString());
-          });
-        }*/
       }
     };
   });
